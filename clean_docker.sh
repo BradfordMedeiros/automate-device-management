adbc6b732d61d1d35a5378a30b630f64b1e2dc0c
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-03-16 01:02:34 -0700

    added test ansible docker

---
+++ clean_docker.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker rmi -f automate_dm
+
--- plugins/automate.js
+++ plugins/automate.js
@@ -1,36 +1,30 @@
 
-let value = 10;
-let status = 'ok';
-
-const mockSlave = {
-  mock_slave: {
-    type: 'mock_slave',
-    get_value: () => value,
-    change_status: newStatus => {
-      status = newStatus;
-    },
-    isValidReachabilityInfo: identification => {
-      return  true;
-    },
-    isValidConfig:  config => {
-      try {
-        const data = JSON.parse(config);
-        return (data.somefield === true || data.somefield == false);
-      }catch(e){
-        return false;
-      }
-    },
-    status: async () => {
-      return status;
-    },
-    config: async configText => {
-      value = value + 1;
-      return true;
-    },
-    commands:  {
-      test: async () => "ok",
-    },
+/*
+  config looks like:
+  {
+    docker_image: <some_image_url>
   }
+
+ */
+
+const automate = {
+  type: 'automate',
+  isValidReachabilityInfo: identification => {
+    return true;
+  },
+  isValidConfig:  config => {
+    return true;
+  },
+  status: async () => {
+    return 'ok';
+  },
+  config: async configText => {
+    return true;
+  },
+  commands:  {
+    unlock: async () => "ok",
+    lock: async () => 'ok',
+  },
 };
 
 module.exports = mockSlave.mock_slave;
