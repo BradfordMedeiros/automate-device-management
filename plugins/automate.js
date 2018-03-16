7a61549c7a58dba3a301acb8aac1c461b1308470
BradfordMedeiros <bradmedeiros0@gmail.com>
2018-03-16 01:18:44 -0700

    automate plugin work, basic but shitty coded config form, mocked out methods, docs

--- plugins/automate.js
+++ plugins/automate.js
@@ -1,5 +1,13 @@
 
 /*
+  reachability info look like:
+  {
+    ip_address: x,
+    user: y,
+    password: password,
+  }
+  sshpass -p "root" ssh root@localhost -p 9000 "echo hello"
+
   config looks like:
   {
     docker_image: <some_image_url>
@@ -10,16 +18,32 @@
 const automate = {
   type: 'automate',
   isValidReachabilityInfo: identification => {
-    return true;
+    try {
+      const data = JSON.parse(identification);
+      return (
+        typeof(data.ip_address) === 'string' &&
+        typeof(data.user) === 'string' &&
+        typeof(data.password) === 'string'
+      )
+    }catch(e){
+      return false;
+    }
   },
   isValidConfig:  config => {
-    return true;
+    try {
+      const data = JSON.parse(config);
+      return (
+        typeof(data.docker_image) === 'string'
+      )
+    }catch(e){
+      return false;
+    }
   },
   status: async () => {
     return 'ok';
   },
   config: async configText => {
-    return true;
+
   },
   commands:  {
     unlock: async () => "ok",
@@ -27,4 +51,4 @@ const automate = {
   },
 };
 
-module.exports = mockSlave.mock_slave;
+module.exports = automate;
