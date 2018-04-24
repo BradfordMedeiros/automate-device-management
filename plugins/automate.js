945574ee3e30c05047bbf3d81ee3db10d6774888
brad <bradmedeiros0@gmail.com>
2018-04-23 23:06:35 -0700

    -

--- plugins/automate.js
+++ plugins/automate.js
@@ -1,15 +1,8 @@
-
 /*
-  reachability info look like:
-  user=root;password=password;host=host;ip=someIp;
-
-  sshpass -p "root" ssh root@localhost -p 9000 "echo hello"
-
-  config looks like:
-  dockerImageName
+    this is intended to be a more minimal version of automate for instances running on general pcs
+    this makes the abilities more limited since we do not have ssh access
 
  */
-
 
 const child_process = require('child_process');
 
@@ -17,65 +10,61 @@ const runCommand = (command, { user, password, ipAddress }) => new Promise((reso
     const commandToExecute = `sshpass -p ${password} ssh -o StrictHostKeyChecking=no ${user}@${ipAddress} ${command}`;
     child_process.exec(commandToExecute, (error, stdout, stderr) => {
         if (error) {
-            console.log(error.toString())
             reject(stderr);
         } else {
-            console.log('no error');
-            console.log(stdout)
-            console.log(stderr)
             resolve(stdout);
         }
     });
 });
 
 const getReachData = reachabilityInfo => {
-  const data = reachabilityInfo.split(';');
-  const mapping = { };
-  data.forEach(item => {
-    const values = item.split('=');
-    mapping[values[0]] = values[1];
-  });
-  return mapping;
+    const data = reachabilityInfo.split(';');
+    const mapping = { };
+    data.forEach(item => {
+        const values = item.split('=');
+        mapping[values[0]] = values[1];
+    });
+    return mapping;
 };
 
-const automate = {
-  type: 'automate',
-  isValidReachabilityInfo: identification => {
-    const reachInfo = getReachData(identification);
-    console.log(reachInfo);
-    return reachInfo.user && reachInfo.password && reachInfo.ipAddress;
-  },
-  isValidConfig:  config => true,
-  status: async reachabilityInfo => {
-    const data = getReachData(reachabilityInfo);
-    try {
-      await runCommand(`'echo hello'`, {
-            user:  data.user,
+const automateFirmware = {
+    type: 'automate',
+    isValidReachabilityInfo: identification => {
+        const reachInfo = getReachData(identification);
+        console.log(reachInfo);
+        return reachInfo.user && reachInfo.password && reachInfo.ipAddress;
+    },
+    isValidConfig:  config => true,
+    status: async reachabilityInfo => {
+        const data = getReachData(reachabilityInfo);
+        try {
+            await runCommand(`'echo hello'`, {
+                user:  data.user,
+                password: data.password,
+                ipAddress: data.ipAddress,
+            });
+            return 'ok';
+        }catch(e){
+            return 'error';
+        }
+    },
+    config: async (configText, reachabilityInfo) => {
+        const image = configText;
+        const data = getReachData(reachabilityInfo);
+        console.log('configuring with: ', image);
+        const isRun = await runCommand({
+            user: data.user,
             password: data.password,
             ipAddress: data.ipAddress,
+            image,
         });
-      return 'ok';
-    }catch(e){
-      return 'error';
-    }
-  },
-  config: async (configText, reachabilityInfo) => {
-    const image = configText;
-    const data = getReachData(reachabilityInfo);
-    console.log('configuring with: ', image);
-    const isRun = await runCommand({
-      user: data.user,
-      password: data.password,
-      ipAddress: data.ipAddress,
-      image,
-    });
-    console.log('finished running');
-    return isRun;
-  },
-  commands:  {
-    unlock: async () => "ok",
-    lock: async () => 'ok',
-  },
+        console.log('finished running');
+        return isRun;
+    },
+    commands:  {
+        unlock: async () => "ok",
+        lock: async () => 'ok',
+    },
 };
 
-module.exports = automate;
+module.exports = automateFirmware;
